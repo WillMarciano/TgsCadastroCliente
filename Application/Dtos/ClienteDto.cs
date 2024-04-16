8969b0a44fdd19292289a3c37b0ae86fbc3186ce
WillMarciano <34887614+WillMarciano@users.noreply.github.com>
2024-04-16 01:10:48 -0300

    Refatorando copdigo retirado acesso entidade da dto

--- Application/Dtos/ClienteDto.cs
+++ Application/Dtos/ClienteDto.cs
@@ -1,6 +1,4 @@
-﻿using Domain.Entity;
-
-namespace Application.Dtos
+﻿namespace Application.Dtos
 {
     public record ClienteDto
     {
@@ -8,7 +6,7 @@ namespace Application.Dtos
         public string? Nome { get; set; }
         public string? Email { get; set; }
         public byte[]? Logotipo { get; set; } 
-        public List<Logradouro>? Logradouros { get; set; }
+        public List<LogradouroDto>? Logradouros { get; set; }
         public DateTime? DataRegistro { get; set; }
     }
 }
